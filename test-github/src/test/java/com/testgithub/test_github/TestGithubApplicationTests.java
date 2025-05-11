e7a7d447c2d77fb318a72c01e3a5aafba1d552ff
gauravpawan89 <gauravabsas1234567@gmail.com>
2025-05-11 17:09:04 +0530

    d

--- test-github/src/test/java/com/testgithub/test_github/TestGithubApplicationTests.java
+++ test-github/src/test/java/com/testgithub/test_github/TestGithubApplicationTests.java
@@ -1,13 +1,9 @@
 package com.testgithub.test_github;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
 class TestGithubApplicationTests {
 
-	@Test
 	void contextLoads() {
+		System.out.println("");
 	}
 
 }
